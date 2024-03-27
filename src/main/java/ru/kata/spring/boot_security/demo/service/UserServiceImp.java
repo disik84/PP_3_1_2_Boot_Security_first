c048f6b8567b52ed29c76180ff31d35b34c79450
disik84 <a9625997777@gmail.com>
2024-03-27 16:17:26 +0300

    16:17 27.03.24

--- src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImp.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImp.java
@@ -1,5 +1,5 @@
 package ru.kata.spring.boot_security.demo.service;
-
+//test
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.jdbc.core.JdbcTemplate;
